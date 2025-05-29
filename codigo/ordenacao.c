0cf50b9ad323c9e623e331b5a720ae1ffadc20d0
Erik Zavan <131304266+erikzavan@users.noreply.github.com>
2025-05-29 14:40:10 -0300

    Update ordenacao.c

--- codigo/ordenacao.c
+++ codigo/ordenacao.c
@@ -1,1 +1,11 @@
+#include <time.h>
 
+
+// para medir o tempo
+
+clock_t inicio = clock();
+// chama a função de ordenação
+clock_t fim = clock();
+
+double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+printf("Tempo de execução: %.2f segundos\n", tempo);
