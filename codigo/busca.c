bc885a963fa05396f79a95347ea96e3ff4b33b09
erikzavan <erikzavan@gmail.com>
2025-06-02 15:01:32 -0300

    carregar dados, bubble sorting

--- codigo/busca.c
+++ codigo/busca.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int* carregar_dados(const char *nome_arquivo, int *tamanho) {
+    //open
+    FILE *arquivo = fopen(nome_arquivo, "rb");
+    if (!arquivo) {
+        exit(EXIT_FAILURE);
+    }
+
+    //size
+    fseek(arquivo, 0, SEEK_END);
+    long tamanho_bytes = ftell(arquivo);
+    rewind(arquivo);
+
+    //alloc
+    *tamanho = tamanho_bytes / sizeof(int);
+    int *dados = (int *)malloc(tamanho_bytes);
+    if (!dados) {
+        exit(EXIT_FAILURE);
+    }
+
+    //read
+    fread(dados, sizeof(int), *tamanho, arquivo);
+    fclose(arquivo);
+
+    return dados;
+}
+
+int busca_sequencial(int *v, int tamanho, int alvo) {
+    for (int i = 0; i < tamanho; i++) {
+        if (v[i] == alvo)
+            return i;
+    }
+    return -1;
+}
+
+int busca_binaria(int *v, int tamanho, int alvo) {
+    int inicio = 0;
+    int fim = tamanho - 1;
+
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+        if (v[meio] == alvo)
+            return meio;
+        else if (v[meio] < alvo)
+            inicio = meio + 1;
+        else
+            fim = meio - 1;
+    }
+    return -1;
+}
--- codigo/ordenacao.c
+++ codigo/ordenacao.c
@@ -1,21 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+void trocar(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// SelectionSort
+
+// BubbleSort (padrao) 
+void bubble_sort(int v[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (v[j] > v[j + 1]) {
+                trocar(&v[j], &v[j + 1]);
+            }
+        }
+    }
+}
+
+// BubbleSort (otimizado)
+void bubble_sort_otimizado(int v[], int n) {
+    int trocou;
+    for (int i = 0; i < n - 1; i++) {
+        trocou = 0;
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (v[j] > v[j + 1]) {
+                trocar(&v[j], &v[j + 1]);
+                trocou = 1;
+            }
+        }
+        if (trocou == 0)
+            break;
+    }
+}
+
+// InsertionSort (padrao)
+// InsertionSort (otimizado)
+
+
 
 
 // para medir o tempo
 
 clock_t inicio = clock();
-// chama a função de ordenação
+// chama a funcao de ordenacao
 clock_t fim = clock();
 
 double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
 printf("Tempo de execução: %.2f segundos\n", tempo);
-
-// SelectionSort
-// BubbleSort (padrão) 
-// BubbleSort (otimizado)
-// InsertionSort (padrão)
-// InsertionSort (otimizado)
-
-int main{
-  return 0;
-}
